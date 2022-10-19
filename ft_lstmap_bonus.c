3802d376fe9b2dcf91da5b46c87911d2715027bc
csantacr <csantacr@student.42.fr>
2022-10-19 20:44:28 +0200

    passing all francinette tests

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,10 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csantacr <csantacr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 15:32:44 by csantacr          #+#    #+#             */
+/*   Updated: 2022/10/19 20:39:15 by csantacr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new;
+	t_list	*aux;
+
 	if (!lst)
 		return (NULL);
-	if (!newcont)
-		ft_lstclear(&newlst, del);
+	new = NULL;
+	while (lst)
+	{
+		aux = ft_lstnew(f(lst->content));
+		if (!aux)
+			ft_lstclear(&new, del);
+		ft_lstadd_back(&new, aux);
+		lst = lst->next;
+	}
+	return (new);
 }
