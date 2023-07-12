899d4fa66de3a16c9ab6196c9ccc2418a6534574
csantacr <csantacr@student.42.fr>
2023-07-12 13:35:26 +0200

    updated returns

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,23 +1,25 @@
 #include "libft.h"
 
-void	ft_putnbr(int nb)
+int	ft_putnbr(int nb)
 {
+	unsigned int	unb;
+
+	unb = 0;
 	if (nb == -2147483648)
 	{
-		ft_putnbr(-214748364);
-		ft_putchar('8');
+		unb += ft_putnbr(-214748364);
+		unb += ft_putchar('8');
 	}
 	else if (nb < 0)
 	{
-		ft_putchar('-');
-		ft_putnbr(-nb);
+		unb += ft_putchar('-');
+		unb += ft_putnbr(-nb);
 	}
 	else
 	{
 		if (nb > 9)
-		{
-			ft_putnbr(nb / 10);
-		}
-		ft_putchar((nb % 10) + '0');
+			unb += ft_putnbr(nb / 10);
+		unb += ft_putchar((nb % 10) + '0');
 	}
+	return (unb);
 }
--- ft_putstr.c
+++ ft_putstr.c
@@ -1,10 +1,15 @@
 #include "libft.h"
 
-void	ft_putstr(char *str)
+int	ft_putstr(char *str)
 {
+	int	bc;
+
+	bc = 0;
 	while (*str)
 	{
 		write(1, str, 1);
 		str++;
+		bc++;
 	}
+	return (bc);
 }
